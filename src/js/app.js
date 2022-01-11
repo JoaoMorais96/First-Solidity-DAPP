b0cb192a89aed09bcaba9257085eb30011880453
JoaoMorais96 <goncalomorais96@hotmail.com>
2022-01-11 11:57:34 +0000

    added initial front end for selling an item

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,18 @@ App = {
      contracts: {},
 
      init: function() {
-          /*
-           * Replace me...
-           */
+        //Load articles
+        var articlesRow =$('#articlesRow');
+        var articleTemplate = $('#articleTemplate');
+
+        //Creating a product to sell
+        articleTemplate.find('.panel-title').text('article 1')
+        articleTemplate.find('.article-description').text('Description for article 1')
+        articleTemplate.find('.article-price').text('10.23');
+        articleTemplate.find('.article-seller').text('0x89273498356981724')
+
+        //Add the articles content to the row of articles (articlesRow)
+        articlesRow.append(articleTemplate.html());
 
           return App.initWeb3();
      },
@@ -27,6 +36,7 @@ App = {
 
 $(function() {
      $(window).load(function() {
+          //Called when the page is open
           App.init();
      });
 });
